efb9aedbfd12d823e834de529228f80a4c396f36
szczepuz999 <szczepuz999@gmail.com>
2021-07-09 19:44:41 +0200

    rest from web scraper project

---
+++ Topics/How to read the documentation/Math from NumPy/main.py
@@ -0,0 +1,6 @@
+# import the required library
+import math
+
+def calculate_cosine(angle_in_degrees):
+    # do not forget to round the result and print it
+    print(round(math.cos(math.radians(angle_in_degrees)), 2))
---
+++ Topics/How to read the documentation/Numeric Types/main.py
@@ -0,0 +1,3 @@
+
+def some_calculate(a, b):
+    print(abs((a % b) - pow(b,a)))
